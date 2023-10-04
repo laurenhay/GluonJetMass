fd8328ceade88dce6965a553e494ebf87e16899f
Lauren Hay <lmhay@buffalo.edu>
2023-10-04 13:07:10 -0400

    added fakes and misses

--- testUnfold.C
+++ testUnfold.C
@@ -21,6 +21,8 @@ void testUnfold()
     TH2D *ptreco_ptgen_g = f->Get<TH2D>("ptreco_ptgen_g");
     TH2D *mreco_mgen_u = f->Get<TH2D>("mreco_mgen_u");
     TH2D *ptreco_ptgen_u = f->Get<TH2D>("ptreco_ptgen_u");
+    TH2D *fakes_ptreco_mreco = f->Get<TH2D>("fakes_ptreco_mreco");
+    TH2D *misses_ptgen_mgen = f->Get<TH2D>("misses_ptgen_mgen");
     //make arrays to store bin centers: 
     //    Double_t ptreco_center_d[10];
 //     Double_t resp_u[10][11][10][21];
@@ -78,21 +80,30 @@ void testUnfold()
 //         massBins_fine[i-1] = mreco_mgen_u->GetXaxis()->GetBinLowEdge(i);
 //         cout << "Bin edge " << massBins_fine[i-1] << endl;;
 //     }
-    TUnfoldBinning *detectorBinning = new TUnfoldBinning(*mreco_mgen_u->GetXaxis(), true, true);
-    detectorBinning->AddAxis(*ptreco_ptgen_u->GetXaxis(), true, true);
+    TUnfoldBinning *detectorBinning = new TUnfoldBinning("detector");
+    TUnfoldBinning *missBinning = detectorBinning->AddBinning("missesBin", 1);
+    TUnfoldBinning *recoBinning = detectorBinning->AddBinning("reco");
+    recoBinning->AddAxis(*mreco_mgen_u->GetXaxis(), false, true);
+    recoBinning->AddAxis(*ptreco_ptgen_u->GetXaxis(), false, true);
     cout << detectorBinning << endl;
     
-    TUnfoldBinning *generatorBinning = new TUnfoldBinning(*mreco_mgen_u->GetYaxis(), true, true);
-    generatorBinning->AddAxis(*ptreco_ptgen_u->GetYaxis(), true, true);
+    TUnfoldBinning *generatorBinning = new TUnfoldBinning("generator");
+    TUnfoldBinning *fakeBinning=generatorBinning->AddBinning("fakesBin", 1);
+    TUnfoldBinning *genBinning = generatorBinning->AddBinning("gen");
+    genBinning->AddAxis(*mreco_mgen_u->GetYaxis(), false, true);
+    genBinning->AddAxis(*ptreco_ptgen_u->GetYaxis(), false, true);
     cout << &generatorBinning << endl;
     
-    cout << "Test binning for ptreco = 250, mreco = 500 " <<detectorBinning->GetGlobalBinNumber(250,500) << endl;
+    cout << "Test binning for ptreco = 250, mreco = 500 " <<recoBinning->GetGlobalBinNumber(250,500) << endl;
     
     TH2D *histMCGenRec=TUnfoldBinning::CreateHistogramOfMigrations(generatorBinning,detectorBinning,"histMCGenRec");
     TH1 *histMCGen =   generatorBinning->CreateHistogram("histMCGen");
     TH1 *histMCReco =   detectorBinning->CreateHistogram("histMCReco");
     int global_bin_ind = 0;
-    
+
+
+
+    // Loop through reco and gen bins and fill Response matrix    
 while(matrixReader.Next() && binReader.Next()) {
         cout << "Type of groomed: " << typeid(response_matrix_g).name() << " and size: " << response_matrix_u.GetSize() << endl;
         cout << "Size of ptreco bins: " << ptreco_center.GetSize()<< endl;
@@ -101,25 +112,34 @@ while(matrixReader.Next() && binReader.Next()) {
         // i is ptreco, j is mreco, k is ptgen, l is mgen
         for(int i=0; i<ptreco_center.GetSize(); i++){
              for(int j=0; j<mreco_center.GetSize(); j++){
-//            for(int j=0; j<1; j++){
                 for(int k=0; k<ptgen_center.GetSize(); k++){
-                     for(int l=0; l<mgen_center.GetSize(); l++){
-//                    for(int l=0; l<1; l++){
-                        Int_t glob_recobin = i*mreco_center.GetSize()+j;
-                        Int_t glob_genbin = k*mgen_center.GetSize()+l;
-                        Int_t glob_bin = glob_recobin*(mgen_center.GetSize()*ptgen_center.GetSize())+glob_genbin;
-                        cout<<"Global bin " << glob_bin << " for reco bin " << glob_recobin << " and gen bin " << glob_genbin << endl;
-                        cout<<"has centers mreco " << mreco_center[j] << " and ptreco " << ptreco_center[i] << endl;
-                        cout<<"has centers mgen " << mgen_center[l] << " and ptreco " << ptgen_center[k] << endl;
-                        Int_t genBin=generatorBinning->GetGlobalBinNumber(mgen_center[l],ptgen_center[k]);
-                        Int_t recoBin=detectorBinning->GetGlobalBinNumber(mreco_center[j],ptreco_center[i]);
-			//                        cout <<"TUnfold gen bin "<< genBin << " and reco bin " << recoBin<< "for value" << response_matrix_u[glob_bin] <<endl;
-                        Double_t resp_weight_u = response_matrix_u[glob_bin];
-                        histMCGenRec->Fill(genBin,recoBin,resp_weight_u);
-			histMCGen->Fill(genBin, resp_weight_u);
-			histMCReco->Fill(recoBin, resp_weight_u);
-                        global_bin_ind++;
-                    }}}}
+		  for(int l=0; l<mgen_center.GetSize(); l++){
+     		    Int_t glob_recobin = i*mreco_center.GetSize()+j;
+		    Int_t glob_genbin = k*mgen_center.GetSize()+l;
+		    Int_t glob_bin = glob_recobin*(mgen_center.GetSize()*ptgen_center.GetSize())+glob_genbin;
+		    cout<<"Global bin " << glob_bin << " for reco bin " << glob_recobin << " and gen bin " << glob_genbin << endl;
+		    cout<<"has centers mreco " << mreco_center[j] << " and ptreco " << ptreco_center[i] << endl;
+		    cout<<"has centers mgen " << mgen_center[l] << " and ptreco " << ptgen_center[k] << endl;
+		    Int_t genBin=genBinning->GetGlobalBinNumber(mgen_center[l],ptgen_center[k]);
+		    Int_t recoBin=recoBinning->GetGlobalBinNumber(mreco_center[j],ptreco_center[i]);
+		    cout <<"TUnfold gen bin "<< genBin << " and reco bin " << recoBin<< "for value" << response_matrix_u[glob_bin] <<endl;
+		    Double_t resp_weight_u = response_matrix_u[glob_bin];
+		    histMCGenRec->Fill(genBin,recoBin,resp_weight_u);
+          	    //only fill fakes in fake genBin
+		    Double_t fake_weight = fakes_ptreco_mreco->GetBinContent(i,j);
+		    cout << "Fake weight " << fake_weight << " for i == " << i << " and j == " << j << endl;
+		    Int_t fakeBin=fakeBinning->GetStartBin();
+		    histMCGenRec->SetBinContent(fakeBin,recoBin,fake_weight);
+                    //only fill misses in miss recoBin                                                                                                                                                             
+                    Double_t miss_weight = misses_ptgen_mgen->GetBinContent(k,l);
+		    cout << "Miss weight " << miss_weight << " for k == " << k << " and k == " << k << endl;
+		    Int_t missBin=missBinning->GetStartBin();
+		    histMCGenRec->SetBinContent(genBin,missBin,miss_weight);
+		    //fill rest of truth and 
+                    histMCGen->Fill(genBin, resp_weight_u);
+		    histMCReco->Fill(recoBin, resp_weight_u);
+		    global_bin_ind++;
+		  }}}}
         cout << response_matrix_u[849] <<endl;              
     }
     
