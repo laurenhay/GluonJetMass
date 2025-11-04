And underflow to regular histograms, fix most of gen weights

python/corrections.py
@@ -400,6 +400,7 @@ def GetJetCorrections(FatJets, events, era, IOV, isData=False, uncertainties = N
     events_cache = events.caches[0]
 
     jet_factory = CorrectedJetsFactory(name_map, jec_stack)
+    print("Fat jets for index 0 ", FatJets)
     corrected_jets = jet_factory.build(FatJets, lazy_cache=events_cache)
     # print("Available uncertainties: ", jet_factory.uncertainties())
     # print("Corrected jets object: ", corrected_jets.fields)
@@ -480,9 +481,9 @@ def GetQ2Weights(events):
 
 
 def GetQ2muF(events):
-    muF = ak.ones_like(events.event)
-    up = ak.ones_like(events.event)
-    down = ak.ones_like(events.event)
+    muF = np.ones(len(events))
+    up = np.ones(len(events))
+    down = np.ones(len(events))
     if ("LHEScaleWeight" in ak.fields(events)):
         if ak.all(ak.num(events.LHEScaleWeight, axis=1)==9):
             nom = events.LHEScaleWeight[:,4]
@@ -491,13 +492,13 @@ def GetQ2muF(events):
         elif ak.all(ak.num(events.LHEScaleWeight, axis=1)==8):
             up = events.LHEScaleWeight[:,4]
             down = events.LHEScaleWeight[:,3]
-    return nom, up, down
+    return muF, up, down
     
 
 def GetQ2muR(events):
-    muR = ak.ones_like(events.event)
-    up = ak.ones_like(events.event)
-    down = ak.ones_like(events.event)
+    muR = np.ones(len(events))
+    up = np.ones(len(events))
+    down = np.ones(len(events))
     if ("LHEScaleWeight" in ak.fields(events)):
         if ak.all(ak.num(events.LHEScaleWeight, axis=1)==9):
             nom = events.LHEScaleWeight[:,7]
@@ -506,7 +507,7 @@ def GetQ2muR(events):
         elif ak.all(ak.num(events.LHEScaleWeight, axis=1)==8):
             up = events.LHEScaleWeight[:,6]
             down = events.LHEScaleWeight[:,1]
-    return nom, up, down
+    return muR, up, down
 
 
 def GetPDFWeights(events):

python/trijetProcessor.py
@@ -111,21 +111,21 @@ class makeTrijetHists(processor.ProcessorABC):
         syst_cat = hist.axis.StrCategory([], growth=True, name='syst', label="Systematic")
         dataset_axis = hist.axis.StrCategory([], growth=True, name="dataset", label="Primary dataset")
         fine_mass_bin = hist.axis.Regular(130, 0.0, 1300.0, name="mass", label=r"mass [GeV]")
-        fine_pt_bin = hist.axis.Regular(500, 100.0, 10100.0, name="pt", label=r"$p_T$ [GeV]")
+        fine_pt_bin = hist.axis.Regular(500, 0.0, 10000.0, name="pt", label=r"$p_T$ [GeV]")
         #### if using specific bin edges use hist.axis.Variable() instead
-        mgen_bin_edges = np.array([0,5,10,20,40,60,80,100,150,200,300, 400, 500, 900,1300])
+        mgen_bin_edges = np.array([0,5,10,20,40,60,80,100,120,140,160,180,200,300, 400, 500, 900,1300])
         mreco_bin_edges = np.sort(np.append(mgen_bin_edges,[(mgen_bin_edges[i]+mgen_bin_edges[i+1])/2 for i in range(len(mgen_bin_edges)-1)]))
         mass_gen_bin =  hist.axis.Variable(mgen_bin_edges, name="mgen", label=r"m_{GEN} (GeV)")                         
         mass_bin = hist.axis.Variable(mreco_bin_edges, name="mreco", label=r"m_{RECO} (GeV)")
         # ptgen_edges = np.array([200,300,390,480,570,680,690,750,810,13000]) #### Old values
-        ptgen_edges = np.array([200,290,400,480,570,680,760,820,13000]) #### NEW VALUES TO SWITCH TO
+        ptgen_edges = np.array([0,200,290,400,480,570,680,760,820,13000]) #### NEW VALUES TO SWITCH TO
         pt_bin = hist.axis.Variable(ptgen_edges, name="ptreco", label=r"p_{T,RECO} (GeV)")  
         pt_gen_bin = hist.axis.Variable(ptgen_edges, name="ptgen", label=r"p_{T,GEN} (GeV)")
         rho_gen_bin = hist.axis.Regular(20, -5.0, 0.0, name="rhogen", label=r"$-\log(\rho^2)_{GEN}$")
         rho_bin = hist.axis.Regular(40, -5.0, 0.0, name="rhoreco", label=r"$-\log(\rho^2)$")
         y_bin = hist.axis.Regular(25, -4.0, 4.0, name="rapidity", label=r"$y$")
         eta_bin = hist.axis.Regular(25, -4., 4., name="eta", label=r"$\eta$")
-        frac_axis = hist.axis.Regular(10, 0.0, 1., name="frac", label="Fraction")
+        frac_axis = hist.axis.Regular(50, 0.0, 1., name="frac", label="Fraction")
         n_axis = hist.axis.Regular(5, 0, 5, name="n", label=r"Number")
         dr_axis = hist.axis.Regular(150, 0, 6.0, name="dr", label=r"$\Delta R$")
         dphi_axis = hist.axis.Regular(150, -np.pi, np.pi, name="dphi", label=r"$\Delta \phi$")
@@ -137,6 +137,8 @@ class makeTrijetHists(processor.ProcessorABC):
             #### For jackknife only need resp. matrix hists
                 'misses_u':                    hist.Hist(dataset_axis, syst_cat, jk_axis, pt_gen_bin, mass_gen_bin, storage="weight", name="Events"),
                 'misses_g':                  hist.Hist(dataset_axis, syst_cat, jk_axis, pt_gen_bin, mass_gen_bin, storage="weight", name="Events"),
+                'passes_u':                   hist.Hist(dataset_axis, syst_cat, jk_axis, pt_gen_bin, mass_gen_bin, storage="weight", name="Events"),
+                'passes_g':                    hist.Hist(dataset_axis, syst_cat, jk_axis, pt_gen_bin, mass_gen_bin, storage="weight", name="Events"),
                 'fakes_u':                     hist.Hist(dataset_axis, syst_cat, jk_axis, pt_bin, mass_bin, storage="weight", name="Events"),
                 'fakes_g':                   hist.Hist(dataset_axis, syst_cat, jk_axis, pt_bin, mass_bin, storage="weight", name="Events"),
                 'underflow':                 hist.Hist(dataset_axis, syst_cat, jk_axis, pt_bin, mass_bin, storage="weight", name="Events"),
@@ -167,10 +169,12 @@ class makeTrijetHists(processor.ProcessorABC):
 
         if not self.jk:
             self._histos.update({ 
-            #### btag study histos
+            #### gluon content/ btag study histos
             'alljet_ptreco_mreco':          hist.Hist(dataset_axis, jet_cat, parton_cat, mass_bin, pt_bin, storage="weight", name="Events"),
-            'MET_over_sumET_pt_reco':    hist.Hist(dataset_axis, syst_cat, frac_axis, pt_bin, storage="weight", label="Events"),
             'btag_eta':                     hist.Hist(dataset_axis, jet_cat, parton_cat, frac_axis, eta_bin, storage="weight", name="Events"),
+            #### MET content histos
+            'MET_over_sumET_pt_reco':       hist.Hist(dataset_axis, syst_cat, frac_axis, pt_bin, storage="weight", label="Events"),
+            'MET_pt_reco':                  hist.Hist(dataset_axis, syst_cat, fine_pt_bin, pt_bin, storage="weight", label="Events"),
             #### Plots of things during the selection process / for debugging
             'njet_gen':                     hist.Hist(dataset_axis, syst_cat, n_axis, storage="weight", label="Events"),
             'njet_reco':                    hist.Hist(dataset_axis, syst_cat, n_axis, storage="weight", label="Events"),
@@ -309,6 +313,13 @@ class makeTrijetHists(processor.ProcessorABC):
             corrected_fatjets = GetJetCorrections(FatJet, events_jk, era, IOV, isData=not self.do_gen)
             corrected_fatjets = corrected_fatjets[corrected_fatjets.subJetIdx1 > -1]
             #print(" Uncorrected subjet mass", events0.SubJet.mass)
+            print(" Uncorrected subjet mass", events_jk.SubJet.mass)
+            print(" Uncorrected jet mass", events_jk.FatJet.mass)
+            print("ak sum of subjet mass ", ak.sum(events_jk.SubJet.mass))
+            print("ak sum of fat jet mass", ak.sum(events_jk.FatJet.mass))
+            if ak.sum(ak.num(events_jk.SubJet.mass)>0)<1:
+                print("No subjets")
+                return out
             corrected_subjets = GetJetCorrections(events_jk.SubJet, events_jk, era, IOV, isData = not self.do_gen, mode = 'AK4')
             corrected_fatjets['msoftdrop'] =   (corrected_subjets[corrected_fatjets.subJetIdx1] + corrected_subjets[corrected_fatjets.subJetIdx2]).mass 
             print("finished correcting mass")
@@ -378,7 +389,7 @@ class makeTrijetHists(processor.ProcessorABC):
                 elif self.do_gen:
                     if "LHEWeight" in events_corr.fields: 
                         #print("Difference between weights calculated from xsdb and LHE :", (events_corr.LHEWeight.originalXWGTUP - getXSweight(dataset, IOV)))
-                        weights = events_corr.LHEWeight.originalXWGTUP
+                        weights = events_corr.LHEWeight.originalXWGTUP * getXSweight(dataset, IOV)
                     else:
                         weights = events_corr.genWeight * getXSweight(dataset, IOV)
                 else:
@@ -591,7 +602,6 @@ class makeTrijetHists(processor.ProcessorABC):
                             out['fakes_eta_phi'].fill(dataset=datastr, syst=jetsyst, phi = fake_jets.phi[~ak.is_none(fake_jets.msoftdrop)], eta = fake_jets.eta[~ak.is_none(fake_jets.msoftdrop)], weight=fake_weights.weight()[~ak.is_none(fake_jets.msoftdrop)])
                     if (jetsyst == "nominal"): 
                         out['cutflow'][datastr]['fakes'] += (len(events_corr[fakes].FatJet))
-                        print("ADDING FAKES TO CUTFLOW")
                     print("Number of events after all selections ", ak.sum(sel.all("genTot_seq", "recoTot_seq", "removeMisses", "removeFakes")))
                     if len(events_corr[sel.all("genTot_seq", "recoTot_seq", "removeMisses", "removeFakes")])<1: 
                         print("No events after all selections and removing fakes & misses")
@@ -689,6 +699,7 @@ class makeTrijetHists(processor.ProcessorABC):
                     if not self.jk:
                         #### plots for checking MET/sumET --> potentially need cut <0.3
                         out["MET_over_sumET_pt_reco"].fill(dataset=datastr,syst=jetsyst, frac=events_corr.MET.pt/events_corr.MET.sumEt, ptreco=jet.pt, weight=self.weights[jetsyst].weight() )
+                        out["MET_pt_reco"].fill(dataset=datastr,syst=jetsyst, pt=events_corr.MET.pt, ptreco=jet.pt, weight=self.weights[jetsyst].weight() )
                         #### plots for checking whether jet veto map is needed
                         out["jet_pt_eta_phi"].fill(dataset=datastr, syst=jetsyst, ptreco=jet.pt, phi=jet.phi, eta=jet.eta, weight=self.weights[jetsyst].weight())
                     weights = self.weights[jetsyst].weight()
@@ -757,6 +768,7 @@ class makeTrijetHists(processor.ProcessorABC):
                     out["rho_reco_g"].fill(dataset=datastr,syst=jetsyst, jk=jk_index, rhoreco=np.log((jet.msoftdrop/jet.pt)**2), weight=self.weights[jetsyst].weight() )
                     if not self.jk:
                         out["MET_over_sumET_pt_reco"].fill(dataset=datastr,syst=jetsyst, frac=events_corr.MET.pt/events_corr.MET.sumEt, ptreco=jet.pt, weight=self.weights[jetsyst].weight() )
+                        out["MET_pt_reco"].fill(dataset=datastr,syst=jetsyst, pt=events_corr.MET.pt, ptreco=jet.pt, weight=self.weights[jetsyst].weight() )
                         out["jet_pt_eta_phi"].fill(dataset=datastr, syst=jetsyst, ptreco=jet.pt, phi=jet.phi, eta=jet.eta, weight=self.weights[jetsyst].weight())
                 print("final jets ", jet)
                 print("final jet pt ", jet.pt)

python/utils.py
@@ -114,24 +114,38 @@ xsdb= { 'QCD_Pt_170to300_TuneCP5_13TeV_pythia8' : 104000.0,
         'QCD_HT1000to1500_TuneCH3_13TeV-madgraphMLM-herwig7': 0.8013,
         'QCD_HT1500to2000_TuneCH3_13TeV-madgraphMLM-herwig7': 0.06815,
         'QCD_HT2000toInf_TuneCH3_13TeV-madgraphMLM-herwig7': 0.01245,
-        'QCD_HT200to300_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8': 1554000.0,
-        'QCD_HT300to500_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8': 323800.0,
-        'QCD_HT500to700_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8': 30280.0,
-        'QCD_HT700to1000_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8':6392.0,
-        'QCD_HT1000to1500_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8': 1118.0,
-        'QCD_HT1500to2000_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8':108.9,
-        'QCD_HT2000toInf_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8': 21.93,
+       #### Values used to produce 2016/2017
+       'QCD_HT100to200_TuneCP5_PSWeights_13TeV-madgraph-pythia8': 23640000.0,
+        'QCD_HT200to300_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8': 1546000.0,
+        'QCD_HT300to500_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8': 321600.0,
+        'QCD_HT500to700_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8': 30250.0,
+        'QCD_HT700to1000_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8': 6364.0,
+        'QCD_HT1000to1500_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8': 1117.0,
+        'QCD_HT1500to2000_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8':108.4,
+        'QCD_HT2000toInf_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8': 22.36,
+       #### Values used to produce 2017/2018 mc
+       'QCD_HT100to200_TuneCP5_13TeV-madgraph-pythia8': 23700000.0,
+       'QCD_HT200to300_TuneCP5_13TeV-madgraphMLM-pythia8' : 1552000.0,
+       'QCD_HT300to500_TuneCP5_13TeV-madgraphMLM-pythia8' :  3221100.0,
+       'QCD_HT500to700_TuneCP5_13TeV-madgraphMLM-pythia8' :   30980.0,
+       'QCD_HT700to1000_TuneCP5_13TeV-madgraphMLM-pythia8' :   6398.0,
+       'QCD_HT1000to1500_TuneCP5_13TeV-madgraphMLM-pythia8' :   1122.0,
+       'QCD_HT1500to2000_TuneCP5_13TeV-madgraphMLM-pythia8' :    109.4,
+       'QCD_HT2000toInf_TuneCP5_13TeV-madgraphMLM-pythia8' :      21.74,
+       
         'QCD_Pt-15to7000_TuneCH3_Flat_13TeV_herwig7': 1329000000.0,
-           
-        'WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8':53940.0,
-       'ZJetsToNuNu_HT-100To200_13TeV-madgraph':93.35,
-       'ZJetsToNuNu_HT-200To400_13TeV-madgraph':25.85,
-       'ZJetsToNuNu_HT-400To600_13TeV-madgraph': 3.584,
-       'ZJetsToNuNu_HT-600To800_13TeV-madgraph': 0.853,
-       'ZJetsToNuNu_HT-800To1200_13TeV-madgraph': 0.3934,
-       'ZJetsToNuNu_HT-1200To2500_13TeV-madgraph':0.09543,
-       'ZJetsToNuNu_HT-2500ToInf_13TeV-madgraph':0.002304,
-       'TTJets_TuneCP5_13TeV-madgraphMLM-pythia8':471.7	
+        #### Backgrounds 
+        'WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8': 53940.0,
+
+       'ZJetsToNuNu_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8':271.3,
+       'ZJetsToNuNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8': 72.69,
+       'ZJetsToNuNu_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8': 9.961,
+       'ZJetsToNuNu_HT-600To800_TuneCP5_13TeV-madgraphMLM-pythia8': 2.425,
+       'ZJetsToNuNu_HT-800To1200_TuneCP5_13TeV-madgraphMLM-pythia8': 1.076,
+       'ZJetsToNuNu_HT-1200To2500_TuneCP5_13TeV-madgraphMLM-pythia8': 0.2474,
+       'ZJetsToNuNu_HT-2500ToInf_TuneCP5_13TeV-madgraphMLM-pythia8': 0.005609,
+
+       'TTJets_TuneCP5_13TeV-madgraphMLM-pythia8': 471.7	,
        
  }
 
@@ -145,47 +159,44 @@ lumi = {'2018' : 59.74,
 
 
 #### Missing 2016APV XS's
-num_gen_mg = { '2016APV': {'QCD_HT200to300_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8' : 0,
-'QCD_Pt_300to470_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_470to600_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_600to800_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_800to1000_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_1000to1400_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_1400to1800_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_1800to2400_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_2400to3200_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_3200toInf_TuneCP5_13TeV_pythia8' : 0},
-    '2016': {'QCD_Pt_170to300_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_300to470_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_470to600_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_600to800_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_800to1000_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_1000to1400_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_1400to1800_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_1800to2400_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_2400to3200_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_3200toInf_TuneCP5_13TeV_pythia8' : 0},
-           '2017': {'QCD_Pt_170to300_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_300to470_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_470to600_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_600to800_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_800to1000_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_1000to1400_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_1400to1800_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_1800to2400_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_2400to3200_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_3200toInf_TuneCP5_13TeV_pythia8' : 0},
-           '2018': {'QCD_Pt_170to300_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_300to470_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_470to600_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_600to800_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_800to1000_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_1000to1400_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_1400to1800_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_1800to2400_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_2400to3200_TuneCP5_13TeV_pythia8' : 0,
-'QCD_Pt_3200toInf_TuneCP5_13TeV_pythia8' : 0}
-}
+sumw_qcd_mg = { '2016APV': {
+        'QCD_HT200to300_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8': 101566726040.5,
+        'QCD_HT300to500_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8':  18372261702.0,
+        'QCD_HT500to700_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8' :  2253983329.5,
+        'QCD_HT700to1000_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8':   409397088.68359375,
+        'QCD_HT1000to1500_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8' :  20934451.69921875,
+        'QCD_HT1500to2000_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8':    1983266.7685546875,
+        'QCD_HT2000toInf_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8':      196907.1982421875, 
+},
+    '2016': {
+        'QCD_HT200to300_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8': 70210797200.0,
+        'QCD_HT300to500_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8': 14630450930.0,
+        'QCD_HT500to700_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8' : 2254432227.65625,
+        'QCD_HT700to1000_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8':  158406238.25,
+        'QCD_HT1000to1500_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8' :  8486996.25390625,
+        'QCD_HT1500to2000_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8':   1361777.333267212,
+        'QCD_HT2000toInf_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8':      67843.4627532959,
+    },
+           '2017': {
+               'QCD_HT200to300_TuneCP5_13TeV-madgraphMLM-pythia8' : 181026146494.0,
+               'QCD_HT300to500_TuneCP5_13TeV-madgraphMLM-pythia8' :  36516294423.5,
+               'QCD_HT500to700_TuneCP5_13TeV-madgraphMLM-pythia8' :   3486166624.203125,
+               'QCD_HT700to1000_TuneCP5_13TeV-madgraphMLM-pythia8' :   563102688.9375,
+               'QCD_HT1000to1500_TuneCP5_13TeV-madgraphMLM-pythia8' :   32239843.666503906,
+               'QCD_HT1500to2000_TuneCP5_13TeV-madgraphMLM-pythia8' :    2451711.3540649414,
+               'QCD_HT2000toInf_TuneCP5_13TeV-madgraphMLM-pythia8' :      248156.31952667236,
+},
+           '2018': {
+               'QCD_HT200to300_TuneCP5_13TeV-madgraphMLM-pythia8' : 181673668336.0,
+               'QCD_HT300to500_TuneCP5_13TeV-madgraphMLM-pythia8' :  35869948198.75,
+               'QCD_HT500to700_TuneCP5_13TeV-madgraphMLM-pythia8' :   3529868167.8984375,
+               'QCD_HT700to1000_TuneCP5_13TeV-madgraphMLM-pythia8' :  575521898.7402344,
+               'QCD_HT1000to1500_TuneCP5_13TeV-madgraphMLM-pythia8' :   33323673.349121094,
+               'QCD_HT1500to2000_TuneCP5_13TeV-madgraphMLM-pythia8' :    2520025.5220947266,
+               'QCD_HT2000toInf_TuneCP5_13TeV-madgraphMLM-pythia8' :      260872.52236175537,
+},
+           }
+ 
 num_gen_herwig = { "2016APV": {'QCD_HT100to200_TuneCH3_13TeV-madgraphMLM-herwig7': 3005498,
                                'QCD_HT200to300_TuneCH3_13TeV-madgraphMLM-herwig7': 1967563,
                                'QCD_HT300to500_TuneCH3_13TeV-madgraphMLM-herwig7': 1000485,
@@ -223,6 +234,7 @@ num_gen_herwig = { "2016APV": {'QCD_HT100to200_TuneCH3_13TeV-madgraphMLM-herwig7
                                'QCD_HT2000toInf_TuneCH3_13TeV-madgraphMLM-herwig7': 492537,
                               },
 }
+
 num_gen_herwig_flat = { 
     '2016': {'QCD_Pt-15to7000_TuneCH3_Flat_13TeV_herwig7' : 53923986},
     '2016APV': {'QCD_Pt-15to7000_TuneCH3_Flat_13TeV_herwig7': 45952213},
@@ -271,7 +283,78 @@ num_gen_pythia = { '2016APV': {'QCD_Pt_170to300_TuneCP5_13TeV_pythia8' : 2788500
 'QCD_Pt_2400to3200_TuneCP5_13TeV_pythia8' : 2931000,
 'QCD_Pt_3200toInf_TuneCP5_13TeV_pythia8' : 1000000}
 }
-        
+
+num_gen_bg = {
+    '2016APV': {
+        'ZJetsToNuNu_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8': 7715405,
+       'ZJetsToNuNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8':  7531529,
+       'ZJetsToNuNu_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8':  6770574,
+       'ZJetsToNuNu_HT-600To800_TuneCP5_13TeV-madgraphMLM-pythia8':  2030858,
+       'ZJetsToNuNu_HT-800To1200_TuneCP5_13TeV-madgraphMLM-pythia8':  703970,
+       'ZJetsToNuNu_HT-1200To2500_TuneCP5_13TeV-madgraphMLM-pythia8': 136393,
+       'ZJetsToNuNu_HT-2500ToInf_TuneCP5_13TeV-madgraphMLM-pythia8':  111838,},
+    '2016': {
+        'ZJetsToNuNu_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8': 7083216,
+       'ZJetsToNuNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8':  6814106,
+       'ZJetsToNuNu_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8':  6114046,
+       'ZJetsToNuNu_HT-600To800_TuneCP5_13TeV-madgraphMLM-pythia8':  1881671,
+       'ZJetsToNuNu_HT-800To1200_TuneCP5_13TeV-madgraphMLM-pythia8':  633500,
+       'ZJetsToNuNu_HT-1200To2500_TuneCP5_13TeV-madgraphMLM-pythia8': 115609,
+       'ZJetsToNuNu_HT-2500ToInf_TuneCP5_13TeV-madgraphMLM-pythia8':  110461,},
+    '2017': {
+        'ZJetsToNuNu_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8': 18948271,
+       'ZJetsToNuNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8':  17189820,
+       'ZJetsToNuNu_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8':  13963690,
+       'ZJetsToNuNu_HT-600To800_TuneCP5_13TeV-madgraphMLM-pythia8':   4418971,
+       'ZJetsToNuNu_HT-800To1200_TuneCP5_13TeV-madgraphMLM-pythia8':  1513585,
+       'ZJetsToNuNu_HT-1200To2500_TuneCP5_13TeV-madgraphMLM-pythia8':  267125,
+       'ZJetsToNuNu_HT-2500ToInf_TuneCP5_13TeV-madgraphMLM-pythia8':   176201,},
+    '2018': {
+        'ZJetsToNuNu_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8': 28876062,
+       'ZJetsToNuNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8':  22749608,
+       'ZJetsToNuNu_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8':  19676607,
+       'ZJetsToNuNu_HT-600To800_TuneCP5_13TeV-madgraphMLM-pythia8':   5968910,
+       'ZJetsToNuNu_HT-800To1200_TuneCP5_13TeV-madgraphMLM-pythia8':  2129122,
+       'ZJetsToNuNu_HT-1200To2500_TuneCP5_13TeV-madgraphMLM-pythia8':  381695,
+       'ZJetsToNuNu_HT-2500ToInf_TuneCP5_13TeV-madgraphMLM-pythia8':   268224,},
+    
+}
+
+sumw_bg = {
+    '2016APV': {
+        'ZJetsToNuNu_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8': 7715405,
+       'ZJetsToNuNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8':  7531529,
+       'ZJetsToNuNu_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8':  6770574,
+       'ZJetsToNuNu_HT-600To800_TuneCP5_13TeV-madgraphMLM-pythia8':  2030858,
+       'ZJetsToNuNu_HT-800To1200_TuneCP5_13TeV-madgraphMLM-pythia8':  703970,
+       'ZJetsToNuNu_HT-1200To2500_TuneCP5_13TeV-madgraphMLM-pythia8': 136393,
+       'ZJetsToNuNu_HT-2500ToInf_TuneCP5_13TeV-madgraphMLM-pythia8':  111838,},
+    '2016': {
+        'ZJetsToNuNu_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8': 7083216,
+       'ZJetsToNuNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8':  6814106,
+       'ZJetsToNuNu_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8':  6114046,
+       'ZJetsToNuNu_HT-600To800_TuneCP5_13TeV-madgraphMLM-pythia8':  1881671,
+       'ZJetsToNuNu_HT-800To1200_TuneCP5_13TeV-madgraphMLM-pythia8':  633500,
+       'ZJetsToNuNu_HT-1200To2500_TuneCP5_13TeV-madgraphMLM-pythia8': 115609,
+       'ZJetsToNuNu_HT-2500ToInf_TuneCP5_13TeV-madgraphMLM-pythia8':  110461,},
+    '2017': {
+        'ZJetsToNuNu_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8': 18948271,
+       'ZJetsToNuNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8':  17189820,
+       'ZJetsToNuNu_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8':  13963690,
+       'ZJetsToNuNu_HT-600To800_TuneCP5_13TeV-madgraphMLM-pythia8':   4418971,
+       'ZJetsToNuNu_HT-800To1200_TuneCP5_13TeV-madgraphMLM-pythia8':  1513585,
+       'ZJetsToNuNu_HT-1200To2500_TuneCP5_13TeV-madgraphMLM-pythia8':  267125,
+       'ZJetsToNuNu_HT-2500ToInf_TuneCP5_13TeV-madgraphMLM-pythia8':   176201,},
+    '2018': {
+        'ZJetsToNuNu_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8': 6299042.111694336,
+       'ZJetsToNuNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8':  1676015.0802001953,
+       'ZJetsToNuNu_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8':  233325.02221679688,
+       'ZJetsToNuNu_HT-600To800_TuneCP5_13TeV-madgraphMLM-pythia8':   13479.285751342773,
+       'ZJetsToNuNu_HT-800To1200_TuneCP5_13TeV-madgraphMLM-pythia8':  2724.645004272461,
+       'ZJetsToNuNu_HT-1200To2500_TuneCP5_13TeV-madgraphMLM-pythia8':  136.23207068443298,
+       'ZJetsToNuNu_HT-2500ToInf_TuneCP5_13TeV-madgraphMLM-pythia8':   2.5148895382881165,},
+    
+}
 
 def getRapidity(p4):
     return 0.5 * np.log(( p4.energy + p4.pz ) / ( p4.energy - p4.pz ))
@@ -292,8 +375,14 @@ def getXSweight(dataset, IOV):
                         else:
                             weight = xs * lum * 1000 / num_gen_herwig_flat[year][process]
                     else:
-                        print("Number of gen events for ", year, " ", process, ": ", num_gen[year][process])
-                        weight = xs * lum * 1000 / num_gen[year][process]
+                        if "QCD" in process and process in sumw_qcd_mg[year].keys():
+                            print("Number of gen events for ", year, " ", process, ": ", sumw_qcd_mg[year][process])
+                            weight = xs * lum * 1000 / sumw_qcd_mg[year][process]
+                        elif process in num_gen_bg[year].keys():
+                            print("Number of gen events for ", year, " ", process, ": ", num_gen_bg[year][process])
+                            weight = xs * lum * 1000 / num_gen_bg[year][process]
+                        else:
+                            print("Don't have xs + sumw values for process", process)
                     return weight
 
 